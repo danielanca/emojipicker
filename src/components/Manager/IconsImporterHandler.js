b964307536fca0486323e3bb74d55f0a73c53d47
Daniel Anca <steptu94@gmail.com>
2021-07-28 12:31:14 +0200

    Update
    Update - errors on loading for the first time the emojilist from localstorage

--- src/components/Manager/IconsImporterHandler.js
+++ src/components/Manager/IconsImporterHandler.js
@@ -2,18 +2,25 @@ function IconsImporterHandler(IconTabSelected) {
 	const emojiDatabase = require("../../assets/emoji_pack/emoji.json");
 	var selectedEmojis = [];
 	var selectedIndex = 0;
-	const MAX_LOAD_LENGTH = 45;
+	const MAX_LOAD_LENGTH = 40;
 
 	if (IconTabSelected.includes("recent")) {
 		let filter_emoji = JSON.parse(localStorage.getItem("emojiDatabase"));
-		filter_emoji.forEach((element) => {
-			// console.log("Object names:" + element.emoji);
-		});
-		filter_emoji.sort(function (a, b) {
-			return b.counter - a.counter;
-		});
-		for (var i = 0; i < MAX_LOAD_LENGTH; i++) {
-			selectedEmojis[i] = filter_emoji[i].emoji;
+		if (filter_emoji != null) {
+			filter_emoji.forEach((element) => {
+				// console.log("Object names:" + element.emoji);
+			});
+			filter_emoji.sort(function (a, b) {
+				return b.counter - a.counter;
+			});
+			for (var i = 0; i < filter_emoji.length; i++) {
+				selectedEmojis[i] = filter_emoji[i].emoji;
+			}
+		} else {
+			let emoji_ARRAY = [];
+			let emojiBuffer = { ID: 0, emoji: "😀", counter: 1 };
+			emoji_ARRAY.push(emojiBuffer);
+			localStorage.setItem("emojiDatabase", JSON.stringify(emoji_ARRAY));
 		}
 	} else {
 		for (var i = 0; i < emojiDatabase.length; i++) {
